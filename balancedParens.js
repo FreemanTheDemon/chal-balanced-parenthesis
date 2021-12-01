71ee2440a973b721b1ed50b8516f8f097d4eb7e1
Freeman Bird <freemanbird@gmail.com>
2021-12-01 11:11:49 -0700

    completed challenge

--- balancedParens.js
+++ balancedParens.js
@@ -5,3 +5,25 @@ let sample3 = "This is () also ) unbalanced."
 let sample4 = "Balanced."
 
 // Write your solution below:
+
+const hasBalancedParens = (str) => {
+    let left = '';
+    let right = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '(') {
+            left += '(';
+        } else if (str[i] === ')') {
+            right += ')';
+        }
+    }
+
+    if (left.length === right.length) {
+        return true;
+    }
+    return false;
+}
+
+console.log(hasBalancedParens(sample1));
+console.log(hasBalancedParens(sample2));
+console.log(hasBalancedParens(sample3));
+console.log(hasBalancedParens(sample4));
